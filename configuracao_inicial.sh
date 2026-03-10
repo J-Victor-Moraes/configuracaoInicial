4a9a5dc1c169b437048bcc28e76c8f34a8f3722a
J-Victor-Moraes <jvarantes00007@gmail.com>
2026-03-10 10:09:52 -0300

    Conf v8 Atualizacao do anydesk, senha ,rede e visualização (falta fazer a energia e a suspensao da tela)

--- configuracao_inicial.sh
+++ configuracao_inicial.sh
@@ -28,13 +28,35 @@ sudo adduser user dialout
 # PERMISSAO USB
 ############################################
 
-sudo chmod -Rf 777 /dev/usb/lp0
+echo "Executando as permissões na porta da impressora"
+sudo chmod -Rf 777 /dev/usb/lp1
+sudo chmod -Rf 777 /dev/ttyS0
+sudo chmod -Rf 777 /dev/ttyS1
+sudo chmod -Rf 777 /dev/ttyACM0
+sudo chmod -Rf 777 /dev/usb/lp0 &&\
+echo "Permissão realizada com sucesso"
 
 ############################################
-# SUDO SEM SENHA
+# EXECUTANDO O ANYDESK AO INICIAR O COMPUTADOR
 ############################################
 
-echo "user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/user
+echo "Executando o anydesk ao iniciar o computador"
+sudo apt remove anydesk -y
+sudo apt update
+sudo apt install anydesk -y
+sudo systemctl enable anydesk
+sudo systemctl start anydesk 
+echo "full@time15" | sudo anydesk --set-password &&\
+echo "Anydesk configurado de forma correta"
+
+############################################
+# INSTALACAO DO SSH E NET-TOOLS
+############################################
+
+echo "Executando a instalação do net-tools e ssh"
+sudo apt install net-tools &&\
+sudo apt install ssh &&\
+echo "Instalação das Ferramentas de rede realizada com sucesso"
 
 ############################################
 # DIRETORIO IMAGENS
@@ -55,6 +77,7 @@ chown -R user:user /home/user/imagens_sistema
 # DEFINIR WALLPAPER
 ############################################
 
+echo "Definindo wallpapper e ícone de menu"
 sudo -u $USER_NAME DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_ID/bus" \
 gsettings set org.cinnamon.desktop.background picture-uri "file://$IMAGE_PATH"
 
@@ -64,14 +87,17 @@ gsettings set org.cinnamon.desktop.background picture-uri "file://$IMAGE_PATH"
 
 sudo -u user sed -i 's|"value": "linuxmint-logo-ring-symbolic"|"value": "/home/user/imagens_sistema/logo.png"|' \
 /home/user/.config/cinnamon/spices/menu@cinnamon.org/0.json
+echo "Finalizado as configurações de imagens"
 
 ############################################
 # INSTALACAO PDV 
 ############################################
 
+echo "Instalação a atualização do PDV"
 wget -O pdv.deb "https://drive.usercontent.google.com/download?id=1TEzZk55Xk5sDWYC_YqhxRamKw-LS5EAc&export=download&authuser=0&confirm=t&uuid=9224594e-c815-4ce2-b645-9afa386817a2&at=AGN2oQ2LrFENKJ1l-McldWSCrq09:1773089697757"
 sudo apt install -y ./pdv.deb &&\
 sudo cp -r /opt/pdv/lib/* /usr/lib &&\
 sudo nano /usr/lib/CONFITLS.INI
+echo "PDV atualizado"
 
 echo "Configuracao finalizada!"
